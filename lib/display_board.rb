55180dec79dd750d499b6a310081c1fa580f3091
Michael Michael <michael.ta.chan@gmail.com>
2019-04-16 11:46:04 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,10 +5,12 @@ def display_board(board)
   rows = Array.new(5)
   
   rows[0] = "#{board[0]}|#{board[1]}|#{board[2]}"
+    rows[1] = "-----------"
   rows[2] = "#{board[3]}|#{board[4]}|#{board[5]}"
+    rows[3] = "-----------"
   rows[4] = "#{board[6]}|#{board[7]}|#{board[8]}"
-  rows[1] = "-----------"
-  rows[3] = "-----------"
+
+
   
   for i in 1..5
     puts rows[i]
