89c1e18839685585215de7d2de04176c9d03b4d7
Michael Michael <michael.ta.chan@gmail.com>
2019-04-16 11:41:59 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  board = [" "," "," "," "," "," "," "," "," "]
   
   rows = Array.new(5) "0, 2, 4 are input rows"
   
